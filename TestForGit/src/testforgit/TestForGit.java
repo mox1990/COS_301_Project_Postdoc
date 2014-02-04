2b62a2c0bbeabb569deb0a793eaac7e82a48dae0
MoX <MoX@Moxy>
2014-02-04 11:17:20 +0200

    Test run

---
+++ TestForGit/src/testforgit/TestForGit.java
@@ -0,0 +1,22 @@
+/*
+ * This file is licensed to the authors stated below
+ * Any unauthrised changes are prohibited.
+ * and open the template in the editor.
+ */
+
+package testforgit;
+
+/**
+ *
+ * @author MoX
+ */
+public class TestForGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
