d75dd7186f5fa4db64e6fbbbc3f1c3c6a99e26bb
TMachaba <tokomachaba@gmail.com>
2014-06-29 20:44:49 +0200

    Working on the user gateway

---
+++ Source_Code/PostDoctoralManagementApplication/PostDoctoralManagementApplication/PostDoctoralManagementApplication-ejb/src/main/java/com/softserve/ejb/ApplicationRenewalLocal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.softserve.ejb;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Carlo
+ */
+@Local
+public interface ApplicationRenewalLocal
+ {
+    
+}
