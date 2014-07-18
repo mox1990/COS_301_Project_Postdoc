cf5f678af2239c5774d7b25244f7a4cab47fe22b
kngako <kngako@ymail.com>
2014-07-18 02:36:16 +0200

    Notification Test

---
+++ Source_Code/PostDoctoralManagementApplication/PostDoctoralManagementApplication/PostDoctoralManagementApplication-ejb/src/test/java/test/softserve/EJBUnitTests/NotificationUnitTest.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test.softserve.EJBUnitTests;
+
+import com.softserve.DBDAO.NotificationJpaController;
+import com.softserve.DBEntities.Notification;
+import com.softserve.ejb.NotificationServiceLocal;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.List;
+import javax.ejb.embeddable.EJBContainer;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import test.softserve.MockEJBClasses.NotificationServiceMockUnit;
+
+/**
+ *
+ * @author kgothatso
+ */
+public class NotificationUnitTest {
+    
+    public NotificationUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of sendBatchNotifications method, of class NotificationService.
+     */
+    @Test
+    public void testSendBatchNotifications() {
+        NotificationServiceMockUnit instance = new NotificationServiceMockUnit();
+        
+        NotificationJpaController mockNotificationJpaController = mock(NotificationJpaController.class);
+        
+        instance.setNDAO(mockNotificationJpaController);
+        
+        Notification mockNotification = mock(Notification.class);
+        List<Notification> mockNotifications = Arrays.asList(mockNotification, mockNotification, mockNotification);;
+        
+        try
+        {
+            instance.sendBatchNotifications(mockNotifications, true);
+            
+            for(Notification aMockNotification: mockNotifications)
+            {
+                verify(mockNotificationJpaController).create(aMockNotification);
+            }
+        }
+        catch (Exception ex)
+        {
+            fail("An exception occured");
+        }
+    }
+
+    /**
+     * Test of sendNotification method, of class NotificationService.
+     */
+    @Test
+    public void testSendNotificationWithoutEmail() {
+        NotificationServiceMockUnit instance = new NotificationServiceMockUnit();
+        
+        NotificationJpaController mockNotificationJpaController = mock(NotificationJpaController.class);
+        
+        instance.setNDAO(mockNotificationJpaController);
+        
+        Notification mockNotification = mock(Notification.class);
+        
+        try
+        {
+            instance.sendNotification(mockNotification, false);
+            verify(mockNotificationJpaController).create(mockNotification);
+        }
+        catch (Exception ex)
+        {
+            fail("An exception occured");
+        }
+    }
+    
+    /**
+     * Test of sendNotification method, of class NotificationService.
+     */
+    @Test
+    public void testSendNotificationWithEmail() {
+        NotificationServiceMockUnit instance = new NotificationServiceMockUnit();
+        
+        NotificationJpaController mockNotificationJpaController = mock(NotificationJpaController.class);
+        
+        instance.setNDAO(mockNotificationJpaController);
+        
+        Notification mockNotification = mock(Notification.class);
+        
+        try
+        {
+            instance.sendNotification(mockNotification, true);
+            verify(mockNotificationJpaController).create(mockNotification);
+        }
+        catch (Exception ex)
+        {
+            fail("An exception occured");
+        }
+    }
+    
+}
---
+++ Source_Code/PostDoctoralManagementApplication/PostDoctoralManagementApplication/PostDoctoralManagementApplication-ejb/src/test/java/test/softserve/MockEJBClasses/NotificationServiceMockUnit.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test.softserve.MockEJBClasses;
+
+import com.softserve.DBDAO.AuditLogJpaController;
+import com.softserve.DBDAO.NotificationJpaController;
+import com.softserve.ejb.NotificationService;
+
+/**
+ *
+ * @author kgothatso
+ */
+public class NotificationServiceMockUnit extends NotificationService {
+    private NotificationJpaController nDAO = null;
+    
+    public void setNDAO(NotificationJpaController nDAO)
+    {
+        this.nDAO = nDAO;
+    }
+    
+    @Override
+    protected NotificationJpaController getNotificationDAO()
+    {
+        return nDAO;
+    }
+}
