92a29e2ba1fde6a3683f3b7a2cb82418822fd4d0
kngako <kngako@ymail.com>
2014-08-26 02:05:56 +0200

    Person Report completed... Application report delayed... A Luta Continua...

--- Source_Code/PostDoctoralManagementApplication/PostDoctoralManagementApplication/PostDoctoralManagementApplication-web/src/main/java/com/softserve/Webapp/sessionbeans/ReportBean.java
+++ Source_Code/PostDoctoralManagementApplication/PostDoctoralManagementApplication/PostDoctoralManagementApplication-web/src/main/java/com/softserve/Webapp/sessionbeans/ReportBean.java
@@ -45,7 +45,11 @@ import org.primefaces.model.StreamedContent;
 public class ReportBean implements Serializable {
     private String reportType = "";
     private String[] applicationTypes = {};
+    
     private String[] personTypes = {};
+    private String[] personAttributes = {};
+    private String[] personAddressAttributes = {};
+    private String[] personCvAttributes = {};
     
     @PostConstruct
     public void init()
@@ -62,6 +66,28 @@ public class ReportBean implements Serializable {
     private final String dris = String.valueOf(com.softserve.constants.PersistenceConstants.SECURITY_ROLE_ID_DRIS_MEMBER);
     private final String postDocComitteeMember = String.valueOf(com.softserve.constants.PersistenceConstants.SECURITY_ROLE_ID_POSTDOCTORAL_COMMITTEE_MEMBER);
     private final String systemAdmin = String.valueOf(com.softserve.constants.PersistenceConstants.SECURITY_ROLE_ID_SYSTEM_ADMINISTRATOR);
+    
+    // Person Address data...
+    private final String ADDRESS_COUNTRY = "country";
+    private final String ADDRESS_PROVINCE = "province";
+    private final String ADDRESS_TOWN_CITY = "town_city";
+    private final String ADDRESS_SURBURB = "suburb";
+    private final String ADDRESS_STREET = "street";
+    private final String ADDRESS_STREET_NUMBER = "streetNumber";
+    private final String ADDRESS_ROOM_NUMBER = "roomNumber";
+    private final String ADDRESS_ZIP_CODE = "zip/postalCode";
+    
+    // Person CV data...
+    private final String CV_ID_NUMBER = "idNumber/passport";
+    private final String CV_DOB = "dateOfBirth";
+    private final String CV_GENDER = "gender";
+    private final String CV_CITIZENSHIP = "citizenship";
+    private final String CV_NRF = "NRF Rating";
+    private final String CV_RACE = "Race";
+    private final String CV_RECENT_INSTITUATION = "Recent Institution";
+    private final String CV_RESEARCH_OUTPUT = "Research Output";
+    private final String CV_OTHER_CONTRIB = "Other Contributions";
+    private final String CV_ADDITIONAL = "Additional Information";
     
     @Inject
     private SessionManagerBean sessionManagerBean;
@@ -93,12 +119,36 @@ public class ReportBean implements Serializable {
         this.applicationTypes = applicationTypes;
     }
 
+    public String[] getPersonAttributes() {
+        return personAttributes;
+    }
+
+    public void setPersonAttributes(String[] personTypes) {
+        this.personAttributes = personTypes;
+    }
+
     public String[] getPersonTypes() {
         return personTypes;
     }
 
     public void setPersonTypes(String[] personTypes) {
         this.personTypes = personTypes;
+    }
+
+    public String[] getPersonAddressAttributes() {
+        return personAddressAttributes;
+    }
+
+    public void setPersonAddressAttributes(String[] personAddressAttributes) {
+        this.personAddressAttributes = personAddressAttributes;
+    }
+
+    public String[] getPersonCvAttributes() {
+        return personCvAttributes;
+    }
+
+    public void setPersonCvAttributes(String[] personCvAttributes) {
+        this.personCvAttributes = personCvAttributes;
     }
     
     public String[] applicationTypeValues()
@@ -119,18 +169,150 @@ public class ReportBean implements Serializable {
         return a;
     }
     
+            private final String P_PROSPECTIVE_FELLOW = "prospectiveFellow";
+            private final String P_RESEARCH_FELLOW = "researchFellow";
+            private final String P_GRANT_HOLDER = "grantHolder";
+    
     public String[] personTypeValues()
+    {
+        String[] a = {
+            P_PROSPECTIVE_FELLOW,
+            P_RESEARCH_FELLOW,
+            P_GRANT_HOLDER
+        };
+        return a;
+    }
+    
+    public String[] personAttribValues()
     {
         String[] a = {
             "title",
             "fullName",
             "surname",
             "email",
-            "telephone",
+            "telephoneNumber",
             "workNumber",
-            "faxNumber"
+            "faxNumber",
+            "cellphoneNumber",
+            "accountStatus"
         };
         return a;
+    }
+    
+    public String[] personAddressAttribValues()
+    {
+        String[] a = {
+            ADDRESS_COUNTRY,
+            ADDRESS_PROVINCE,
+            ADDRESS_TOWN_CITY,
+            ADDRESS_SURBURB,
+            ADDRESS_STREET,
+            ADDRESS_STREET_NUMBER,
+            ADDRESS_ROOM_NUMBER,
+            ADDRESS_ZIP_CODE
+        };
+        return a;
+    }
+    
+    private String getAddressAttribFromPerson(String attrib, Person p)
+    {
+        if(p.getAddressLine1() == null) return "null";
+        switch(attrib)
+        {
+            case ADDRESS_COUNTRY:
+                return p.getAddressLine1().getCountry();
+            case ADDRESS_PROVINCE:
+                return p.getAddressLine1().getProvince();
+            case ADDRESS_TOWN_CITY:
+                return p.getAddressLine1().getTownCity();
+            case ADDRESS_SURBURB:
+                return p.getAddressLine1().getSuburb();
+            case ADDRESS_STREET:
+                return p.getAddressLine1().getStreet();
+            case ADDRESS_STREET_NUMBER:
+                return (p.getAddressLine1().getStreeNumber() != null)? p.getAddressLine1().getStreeNumber().toString() : "null";
+            case ADDRESS_ROOM_NUMBER:
+                return p.getAddressLine1().getRoomNumber();
+            case ADDRESS_ZIP_CODE:
+                return p.getAddressLine1().getZippostalCode();
+            default:
+                return "null";
+        }
+    }
+    
+    public String[] personCvAttribValues()
+    {
+        String[] a = {
+            CV_ID_NUMBER,
+            CV_DOB,
+            CV_GENDER,
+            CV_CITIZENSHIP,
+            CV_NRF,
+            CV_RACE,
+            CV_RECENT_INSTITUATION,
+            CV_RESEARCH_OUTPUT,
+            CV_OTHER_CONTRIB,
+            CV_ADDITIONAL
+        };
+        return a;
+    }
+    
+    private String getCvAttribFromPerson(String attrib, Person p)
+    {
+        if(p.getCv() == null) return "null";
+        
+        switch(attrib)
+        {
+            case CV_ID_NUMBER:
+                return p.getCv().getIdNumber();
+            case CV_DOB:
+                return p.getCv().getDateOfBirth().toString();
+            case CV_GENDER:
+                return p.getCv().getGender();
+            case CV_CITIZENSHIP:
+                return p.getCv().getCitizenship();    
+            case CV_NRF:
+                return p.getCv().getNrfRating();
+            case CV_RACE:
+                return p.getCv().getRace();
+            case CV_RECENT_INSTITUATION:
+                return p.getCv().getRecentInstitution();
+            case CV_RESEARCH_OUTPUT:
+                return p.getCv().getResearchOutput();
+            case CV_OTHER_CONTRIB:
+                return p.getCv().getOtherContributions();
+            case CV_ADDITIONAL:
+                return p.getCv().getAdditionalInformation();
+            default:
+                return "null";
+        }
+    }
+    
+    private String getAttribFromPerson(String attrib, Person p)
+    {
+        switch(attrib)
+        {
+            case "title":
+                return p.getTitle();
+            case "fullName":
+                return p.getFullName();
+            case "surname":
+                return p.getSurname();
+            case "email":
+                return p.getEmail();
+            case "telephoneNumber":
+                return p.getTelephoneNumber();
+            case "workNumber":
+                return p.getWorkNumber();
+            case "faxNumber":
+                return p.getFaxNumber();
+            case "cellphoneNumber":
+                return p.getCellphoneNumber();
+            case "accountStatus":
+                return p.getAccountStatus();
+            default:
+                return "null";
+        }
     }
 
     // Person values
@@ -171,7 +353,7 @@ public class ReportBean implements Serializable {
     }
     
     public StreamedContent getReportPDF() // TODO: Add report filters...
-    {
+        {
         InputStream stream = null;
         try 
         {
@@ -193,49 +375,41 @@ public class ReportBean implements Serializable {
                     }
                     break;
                 case "person":
-                    if(personTypes.length == 0 || personTypes.length == 9)
+                    List<Person> persons = new ArrayList<>();
+                    List<List<String>> rows = new ArrayList<List<String>>();
+                    for(String role: personTypes)
                     {
-                        stream = new ByteArrayInputStream(reportEJB.exportAllPersonsToPdf(sessionManagerBean.getSession()));
-                    }
-                    else
-                    {
-                        List<Person> persons = new ArrayList<>();
-                        List<List<String>> rows = new ArrayList<List<String>>();
-                        for(Person p: reportEJB.getAllPersonsWithSecurityRole(com.softserve.constants.PersistenceConstants.SECURITY_ROLE_ID_PROSPECTIVE_FELLOW))
+                        for(Person p: reportEJB.getAllPersonsWithSecurityRole(getSecurityRoleID(role)))
                         {
                             List<String> row = new ArrayList<>();
-                            for(String attrib: personTypes)
+                            for(String attrib: personAttributes)
                             {
                                 String val = getAttribFromPerson(attrib, p);
+                                row.add(val);
+                            }
+                            for(String attrib: personAddressAttributes)
+                            {
+                                String val = getAddressAttribFromPerson(attrib, p);
+                                row.add(val);
+                            }
+                            for(String attrib: personCvAttributes)
+                            {
+                                String val = getCvAttribFromPerson(attrib, p);
                                 row.add(val);
                             }
                             if(!rows.contains(row))
                                 rows.add(row);
                         }
-                        
-                        for(Person p: reportEJB.getAllPersonsWithSecurityRole(com.softserve.constants.PersistenceConstants.SECURITY_ROLE_ID_RESEARCH_FELLOW))
-                        {
-                            List<String> row = new ArrayList<>();
-                            for(String attrib: personTypes)
-                            {
-                                String val = getAttribFromPerson(attrib, p);
-                                row.add(val);
-                            }
-                            rows.add(row);
-                        }
-                        
-                        for(Person p: reportEJB.getAllPersonsWithSecurityRole(com.softserve.constants.PersistenceConstants.SECURITY_ROLE_ID_GRANT_HOLDER))
-                        {
-                            List<String> row = new ArrayList<>();
-                            for(String attrib: personTypes)
-                            {
-                                String val = getAttribFromPerson(attrib, p);
-                                row.add(val);
-                            }
-                            rows.add(row);
-                        }
-                        stream = new ByteArrayInputStream(reportEJB.dynamicReport(Arrays.asList(personTypes), rows));
                     }
+                    List<String> col = new ArrayList<>();
+                    col.addAll(Arrays.asList(personAttributes));
+                    
+                    col.addAll(Arrays.asList(personAddressAttributes));
+                    
+                    col.addAll(Arrays.asList(personCvAttributes));
+                    
+                    stream = new ByteArrayInputStream(reportEJB.dynamicReport(col, rows));
+
                     break;
                 default:
                     throw new Exception("Didn't Specify valid report type.");
@@ -263,31 +437,23 @@ public class ReportBean implements Serializable {
         return null;
     }
     
-    private String getAttribFromPerson(String attrib, Person p)
+    private Long getSecurityRoleID(String role)
     {
-        switch(attrib)
+        switch(role)
         {
-            case "title":
-                return p.getTitle();
-            case "fullName":
-                return p.getFullName();
-            case "surname":
-                return p.getSurname();
-            case "email":
-                return p.getEmail();
-            case "telephone":
-                return p.getTelephoneNumber();
-            case "workNumber":
-                return p.getWorkNumber();
-            case "faxNumber":
-                return p.getFaxNumber();
+            case P_PROSPECTIVE_FELLOW:
+                return com.softserve.constants.PersistenceConstants.SECURITY_ROLE_ID_PROSPECTIVE_FELLOW;
+            case P_RESEARCH_FELLOW:
+                return com.softserve.constants.PersistenceConstants.SECURITY_ROLE_ID_RESEARCH_FELLOW;
+            case P_GRANT_HOLDER:
+                return com.softserve.constants.PersistenceConstants.SECURITY_ROLE_ID_GRANT_HOLDER;
             default:
-                return "null";
+                return Long.MAX_VALUE;
         }
     }
     
     public StreamedContent getReportExcel() // TODO: Add report filters...
-    {
+        {
         InputStream stream = null;
         try 
         {
@@ -310,7 +476,7 @@ public class ReportBean implements Serializable {
                     }
                     break;
                 case "person":
-                    if(personTypes.length == 0 || personTypes.length == 9)
+                    if(personAttributes.length == 0 || personAttributes.length == 9)
                     {
                         stream = new ByteArrayInputStream(reportEJB.exportAllPersonsToExcel(sessionManagerBean.getSession()));
                     }
